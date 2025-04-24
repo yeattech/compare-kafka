d08f805cf7472ba51e8b1224aed3e90a11cb10d0
Yeat Nai Cheng <yeatnaicheng1@gmail.com>
2025-04-24 22:44:39 +0800

    Change consumer to docker configuration of connection to kafka

--- consumer/src/main/java/com/comparison/consumer/entity/Customer.java
+++ consumer/src/main/java/com/comparison/consumer/entity/Customer.java
@@ -2,9 +2,11 @@ package com.comparison.consumer.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @Data
+@Document(collection = "my_customer")
 public class Customer {
     String customerName;
     String contactNumber;
